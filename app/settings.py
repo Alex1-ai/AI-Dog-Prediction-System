751b62c6cc2d0dab9446c3874458f286f5864dab
Alex1.ai <alexanderemmanuel1719@gmail.com>
2025-02-16 21:07:35 +0100

    removed some conflicting words

--- app/settings.py
+++ app/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY= os.environ.get('SECRET_KEY', "secret-key-if-not-configured-in-enviro
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get('DEBUG')
 ALLOWED_HOSTS = [".onrender.com", "https://dog-breed-prediction-system.onrender.com", "*" ]
-CSRF_TRUSTED_ORIGINS = ["https://*.onrender.com" , ".onrender.com", 'https://dog-breed-prediction-system.onrender.com']
+CSRF_TRUSTED_ORIGINS = ["https://*.onrender.com", 'https://dog-breed-prediction-system.onrender.com']
 # ALLOWED_HOSTS = [".fly.dev","https://newsapp27-db.fly.dev/","https://newsapp27.fly.dev", "localhost", "127.0.0.1",
 # ]
 # CSRF_TRUSTED_ORIGINS = ["https://*.fly.dev","https://newsapp27.fly.dev", "https://newsapp27-db.fly.dev/"]
